965e587d2b90d1d7364341b412b5740ca19a88be
codezoneo <anuragtri83@gmail.com>
2024-03-17 21:35:54 +0530

    Change the Functionality

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,24 +1,64 @@
 import React from "react";
+import { useState } from "react";
 
 export default function AddContact() {
+    const [contactData, setContactData] = useState({
+      name: "",
+      email: "",
+      Number: "",
+    });
+
+    const handleChange = (e)=>{
+     if(e.target.name==="name"){
+        setContactData({...contactData, name:e.target.value})
+     }else{
+        setContactData({ ...contactData, email: e.target.value });
+     }
+     
+    }
+
+    const handleAdd = ()=>{
+        if (
+          contactData.name === "" ||
+          contactData.email === "" 
+        ) {
+          alert("please Enter the Details");
+          return;
+        } else {
+            console.log(contactData)
+        }
+    }
+
   return (
-    <div className="formHeader">
-      <div className="add-contact">Add Contacts</div>
-      <form>
-        <label>Name</label>
-        <br />
-        <input type="text" placeholder="Enter Name" name="name" />
-        <br />
-        <label>Email</label>
-        <br />
-        <input type="email" placeholder="Enter Email" name="Email" />
-        <br />
-        <label>Contact Number</label>
-        <br />
-        <input type="text" placeholder="" name="Number" />
-        <br />
-      </form>
-      <button className="btn">Add Contact</button>
-    </div>
+    <>
+      <div className="formHeader">
+        <div className="add-contact">Add Contacts</div>
+        <form>
+          <label>Name</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Name"
+            name="name"
+            value={contactData.name}
+            onChange={handleChange}
+          />
+          <br />
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            name="Email"
+            value={contactData.email}
+            onChange={handleChange}
+          />
+          <br />
+          
+          
+        </form>
+        <button className="btn" onClick={handleAdd}>Add Contact</button>
+      </div>
+    </>
   );
 }
