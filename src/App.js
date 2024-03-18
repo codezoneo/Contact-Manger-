23cfa1201b8e68b113479f6b8d2429aeb4ac0593
codezoneo <anuragtri83@gmail.com>
2024-03-18 22:11:32 +0530

    Finally add in local storage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-
-import { useState } from 'react';
-import './App.css';
-import AddContact from './components/AddContact';
-import ContactList from './components/ContactList';
-import Header from './components/Header';
+import { useState, useEffect } from "react";
+import "./App.css";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
+import Header from "./components/Header";
 import uuid4 from "uuid4";
 
 function App() {
-  
-  const [contact, setContact] = useState([]);
-  const addContact = (data)=>{
-    setContact([...contact,{id:uuid4(),data}])
-  }
-  const removeList = (id)=>{
-     const updatedList = contact.filter((val)=>{
-       return val.id !== id
-     })
-     setContact(updatedList)
-  }
+  const localStorageKey = "contact";
+  const [contact, setContact] = useState(() => {
+    return JSON.parse(localStorage.getItem(localStorageKey)) || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contact));
+  }, [contact]);
+  const addContact = (data) => {
+    setContact([...contact, { id: uuid4(), data }]);
+  };
+  const removeList = (id) => {
+    const updatedList = contact.filter((val) => {
+      return val.id !== id;
+    });
+    setContact(updatedList);
+  };
   return (
-    <div >
+    <div>
       <Header />
-      <AddContact contactAdd ={addContact}/>
-      <ContactList contact={contact} removeContact={removeList}/>
+      <AddContact contactAdd={addContact} />
+      <ContactList contact={contact} removeContact={removeList} />
     </div>
   );
 }
