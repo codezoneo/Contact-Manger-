2641476ff14cd9bed4af5d4f5cbfababd56f28fd
codezoneo <anuragtri83@gmail.com>
2024-03-18 16:27:48 +0530

    Delete functionality add

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import './App.css';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import Header from './components/Header';
+import uuid4 from "uuid4";
 
 function App() {
+  
+  const [contact, setContact] = useState([]);
   const addContact = (data)=>{
-    setContact([...contact,data])
+    setContact([...contact,{id:uuid4(),data}])
   }
-
-  const [contact,setContact] = useState([])
+  const removeList = (id)=>{
+     const updatedList = contact.filter((val)=>{
+       return val.id !== id
+     })
+     setContact(updatedList)
+  }
   return (
     <div >
       <Header />
       <AddContact contactAdd ={addContact}/>
-      <ContactList contact={contact}/>
+      <ContactList contact={contact} removeContact={removeList}/>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,12 +1,17 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function ContactList({ contact }) {
+export default function ContactList(props) {
+  const { contact, removeContact } = props;
   const contactList = contact.map((val) => {
     return (
       <>
         <div className="contacts">
-          <div>{val.name}</div>
-          <div>{val.email}</div>
+          <div style={{ marginLeft: "2px" }}>{val.data.name}</div>
+          <div className="email">{val.data.email}</div>
+          <span onClick={() => removeContact(val.id)}>
+            <DeleteIcon />
+          </span>
         </div>
       </>
     );
